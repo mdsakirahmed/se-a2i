f62817093c2bf83e9902c26bb8cbbc056bacdaf7
Md. Sakir Ahmed <52326976+sakir-developer@users.noreply.github.com>
2022-07-28 15:19:14 +0600

    chart table

--- resources/views/layouts/backend/app.blade.php
+++ resources/views/layouts/backend/app.blade.php
@@ -42,6 +42,11 @@
     <script src="https://code.highcharts.com/modules/accessibility.js"></script>
     {{-- highcharts end --}}
 
+    {{-- MODAL
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> --}}
+
     @livewireStyles
 </head>
 
@@ -233,8 +238,6 @@
 
     <!-- Template Main JS File -->
     <script src="{{ asset('assets/js/main.js') }}"></script>
-
-
 
 
 
--- resources/views/widgets/chart1.blade.php
+++ resources/views/widgets/chart1.blade.php
@@ -49,14 +49,38 @@
     </style>
     <figure class="highcharts-figure">
         <div id="{{ $chart_id }}"></div>
-        <p class="highcharts-description">
-            Chart showing stacked columns with grouping, allowing specific series to
-            be stacked on the same column. Stacking is often used to visualize
-            data that accumulates to a sum.
+        <p class="text-center">
+            <button type="butto m-2" class="btn btn-secondary btn-sm">Edit</button>
         </p>
     </figure>
-
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#chart_modal" id="chart_edit_btn">
+    Launch demo modal
+  </button>
+  
+  <!-- Modal -->
+  <div class="modal fade" id="chart_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
     <script>
         Highcharts.chart("{{ $chart_id }}", {!! collect($chart_data_set) !!});
     </script>
+
+
 </div>
