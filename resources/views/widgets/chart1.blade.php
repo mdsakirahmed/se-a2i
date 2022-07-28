aa2bfc176f13b1c37e988f5e4e536d5860be585b
Md. Sakir Ahmed <52326976+sakir-developer@users.noreply.github.com>
2022-07-28 16:42:24 +0600

    Update chart1.blade.php

--- resources/views/widgets/chart1.blade.php
+++ resources/views/widgets/chart1.blade.php
@@ -46,6 +46,7 @@
         .highcharts-data-table tr:hover {
             background: #f1f7ff;
         }
+
     </style>
     <figure class="highcharts-figure">
         <div id="{{ $chart_id }}"></div>
@@ -55,26 +56,29 @@
     </figure>
 
     <script>
-        Highcharts.chart("{{ $chart_id }}", {!! collect($chart_data_set) !!});
+        Highcharts.chart("{{ $chart_id }}", {
+            !!collect($chart_data_set) !!
+        });
+
     </script>
 
     {{-- Modal Start--}}
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
     {{-- Modal End--}}
 </div>
