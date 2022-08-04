fe546470cbff1ba8f9b187e0f94e07a346da11d9
Md. Sakir Ahmed <52326976+sakir-developer@users.noreply.github.com>
2022-08-04 15:21:24 +0600

    economy/banking-and-finance

--- resources/views/backend/economy/banking-and-finance.blade.php
+++ resources/views/backend/economy/banking-and-finance.blade.php
@@ -9,13 +9,13 @@
                 </h3>
             </header>
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-6">
                     @livewire('chart28')
                 </div>
-                <div class="col-md-12">
+                <div class="col-md-6">
                     @livewire('chart29')
                 </div>
-                <div class="col-md-12">
+                <div class="col-md-6">
                     @livewire('chart30')
                 </div>
             </div>
