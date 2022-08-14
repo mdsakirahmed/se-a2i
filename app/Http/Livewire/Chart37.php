d19250c1fdea9fadb823813d121a89d1505a47af
Md. Sakir Ahmed <52326976+sakir-developer@users.noreply.github.com>
2022-08-14 10:27:51 +0600

    map rendering

--- app/Http/Livewire/Chart37.php
+++ app/Http/Livewire/Chart37.php
@@ -8,96 +8,70 @@ use Livewire\Component;
 
 class Chart37 extends Component
 {
-    public  Chart $chart;
-    public $name, $description, $chart_id = 37;
+  public  Chart $chart;
+  public $name, $description, $chart_id = 37;
 
-    public function render()
-    {
-        $this->chart =Chart::findOrFail($this->chart_id);
-        if(app()->currentLocale() == 'bn'){
-            $this->name = $this->chart->bn_name;
-            $this->description = $this->chart->bn_description;
-        }else{
-            $this->name = $this->chart->en_name;
-            $this->description = $this->chart->en_description;
-        }
-
-        return view('livewire.chart37', [
-            'chart_data_set' => $this->get_data(),
-        ]);
+  public function render()
+  {
+    $this->chart = Chart::findOrFail($this->chart_id);
+    if (app()->currentLocale() == 'bn') {
+      $this->name = $this->chart->bn_name;
+      $this->description = $this->chart->bn_description;
+    } else {
+      $this->name = $this->chart->en_name;
+      $this->description = $this->chart->en_description;
     }
 
-    public function get_data()
-    {
-        $data = json_decode(file_get_contents(public_path('assets/json/bangladesh.geojson.json')), true); 
+    return view('livewire.chart37', [
+      'chart_data_set' => $this->get_data(),
+    ]);
+  }
 
-        // $data = collect($data['features']);
-        // $data = ;
+  public function get_data()
+  {
+    $geojson = json_decode(file_get_contents(public_path('assets/json/bangladesh.geojson.json')), true);
 
+    return [
+      'chart' => [
+        'map' => collect($geojson)
+      ],
 
-        $data = [
-          'crs' => `{type: 'name', properties: {name: 'urn:ogc:def:crs:EPSG::31493'}}`,
-          'features'=> collect($data['features']),
-          'type'=> "FeatureCollection"
-      ];
+      'title' => [
+        'text' => "GeoJSON in Highmaps"
+      ],
 
-        return [
-            'chart' => [
-                'map' => collect($data)
-              ],
-          
-              'title' => [
-                'text' => "GeoJSON in Highmaps"
-              ],
-          
-              'accessibility' => [
-                'typeDescription' => "Map of Germany."
-              ],
-          
-              'mapNavigation' => [
-                'enabled' => true,
-                'buttonOptions' => [
-                  'verticalAlign' => "bottom"
-                ]
-              ],
-          
-              'colorAxis' => [
-                'tickPixelInterval' => 100
-              ],
-          
-              'series' => [
-                [
-                  'data' => [
-                        ['DE.SH', 728],
-                      ['DE.BE', 710],
-                      ['DE.MV', 963],
-                      ['DE.HB', 541],
-                      ['DE.HH', 622],
-                      ['DE.RP', 866],
-                      ['DE.SL', 398],
-                      ['DE.BY', 785],
-                      ['DE.SN', 223],
-                      ['DE.ST', 605],
-                      ['DE.NW', 237],
-                      ['DE.BW', 157],
-                      ['DE.HE', 134],
-                      ['DE.NI', 136],
-                      ['DE.TH', 704],
-                  ],
-                  'keys' => ["code_hasc", "value"],
-                  'joinBy' => "code_hasc",
-                  'name' => "Random data",
-                  'states' => [
-                    'hover' => [
-                      'color' => "#a4edba"
-                  ]
-                    ],
-                  'dataLabels' => [
-                    'enabled' => true,
-                    'format' => "{point.properties.NAME_1}"
-                  ]
-                ]
-              ]
-        ];
-    }
+      'accessibility' => [
+        'typeDescription' => "Map of Germany."
+      ],
+
+      'mapNavigation' => [
+        'enabled' => true,
+        'buttonOptions' => [
+          'verticalAlign' => "bottom"
+        ]
+      ],
+
+      'colorAxis' => [
+        'tickPixelInterval' => 100
+      ],
+
+      'series' => [
+        [
+          'data' => [],
+          'keys' => ["code_hasc", "value"],
+          'joinBy' => "code_hasc",
+          'name' => "Random data",
+          'states' => [
+            'hover' => [
+              'color' => "#a4edba"
+            ]
+          ],
+          'dataLabels' => [
+            'enabled' => true,
+            'format' => "{point.properties.NAME_4}"
+          ]
+        ]
+      ]
+    ];
+  }
 }
