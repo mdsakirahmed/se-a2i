5ad2a75e917b3c22cf04f3d5d7702e5d054a1bf8
Md. Sakir Ahmed <52326976+sakir-developer@users.noreply.github.com>
2022-08-16 14:23:36 +0600

    Update edit-chart.blade.php

--- resources/views/livewire/edit-chart.blade.php
+++ resources/views/livewire/edit-chart.blade.php
@@ -68,54 +68,15 @@
             // $('#bn_description').html("existingValue");
 
 
-            // $('#bn_description').summernote({
-            //     placeholder: 'Bangla description',
-            //     tabsize: 2,
-            //     height: 120,
-            //     callbacks: {
-            //         onChange: function(e) {
-            //             @this.set('bn_description', e);
-            //         }
-            //     }
-            // });
-
-            window.contentfulExtension.init(function (extension) {
-            extension.window.startAutoResizer()
-
-            var existingValue = extension.field.getValue();
-            $('#bn_description').html(existingValue);
-
             $('#bn_description').summernote({
-                height: 300, // set editor height
-                minHeight: null, // set minimum height of editor
-                maxHeight: null, // set maximum height of editor
-                focus: true,
-                toolbar: [
-                    ['style', ['bold', 'italic', 'underline', 'clear']],
-                    ['font', ['strikethrough']],
-                    ['fontsize', ['fontsize']],
-                    ['insert',['picture', 'link']],
-                    ['color', ['color']],
-                    ['para', ['ul', 'ol', 'paragraph', 'style']],
-                    ['height', ['height']],
-                    ['code',['codeview']]
-                ],
-            callbacks: {
-            onInit: function() {
-                console.log('Summernote is launched');
-            },
-            onChange: function(contents, $editable) {
-                var markupStr = $(this).summernote('code');
-                extension.field.setValue(markupStr);
-
-            },
-            onBlur: function() {
-                var markupStr = $(this).summernote('code');
-                extension.field.setValue(markupStr);
-            }
-            }
-            });
-
+                placeholder: 'Bangla description',
+                tabsize: 2,
+                height: 120,
+                callbacks: {
+                    onChange: function(e) {
+                        @this.set('bn_description', e);
+                    }
+                }
             });
 
             $('#en_description').summernote({
