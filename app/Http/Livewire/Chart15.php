a06892cc57536c81d77724a48687bb4668c4e3ca
Md. Sakir Ahmed <52326976+sakir-developer@users.noreply.github.com>
2022-08-16 11:36:40 +0600

    chart 15
    Replace Tableau to highchart

--- app/Http/Livewire/Chart15.php
+++ app/Http/Livewire/Chart15.php
@@ -3,13 +3,25 @@
 namespace App\Http\Livewire;
 
 use App\Models\Chart;
+use App\Models\SecondDatabase\EconomyGdpCompositionSpecific;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
+use Illuminate\Support\Str;
 
 class Chart15 extends Component
 {
     public  Chart $chart;
     public $name, $description, $chart_id = 15;
+    public $chart_type = 'column';
+
+    public $chart_data_set = [];
+    public $broad_sectors = [];
+    public $selected_broad_sector = 'All';
+
+
+    public function mount(){
+        $this->chart_data_set = $this->get_data();
+    }
 
     public function render()
     {
@@ -23,5 +35,76 @@ class Chart15 extends Component
         }
 
         return view('livewire.chart15');
+    }
+
+    public function get_data()
+    {
+        $data = EconomyGdpCompositionSpecific::select('fiscal_year', 'broad_sector', 'specific_sector', 'share_gdp_constant')->get();
+        $this->broad_sectors = $data->pluck('broad_sector')->unique();
+
+        if ($this->selected_broad_sector == 'All') {
+            // Query for all 
+
+        } else {
+            //Query for selected 
+
+        }
+        
+        $series = [];
+        foreach($data->groupBy('specific_sector') as $specific_sector => $specific_sector_wise_data){
+            array_push($series, [
+                'name'=> Str::limit($specific_sector, 20, '......'),
+                'data'=> $specific_sector_wise_data->pluck('share_gdp_constant')
+            ]);
+        }
+
+        $categories = [];
+        foreach ($data->pluck('fiscal_year')->unique() as $fiscal_year){
+            array_push($categories, $fiscal_year);
+        }
+
+        return [
+            'chart'=> [
+                'type' =>  $this->chart_type
+            ],
+
+            'credits' => [
+                'enabled'=>false
+            ],
+            
+            'title'=> [
+                'text'=> ''
+            ],
+            'xAxis'=> [
+                'categories'=> $categories
+            ],
+            'yAxis'=> [
+                'min'=> 0,
+                'title'=> [
+                    'text'=> 'Percentage of Economy (%)'
+                ]
+            ],
+            'legend'=> [
+                'reversed'=> true
+            ],
+            'plotOptions'=> [
+                'column'=> [
+                    'stacking'=> 'normal'
+                ]
+            ],
+            'colors'=> ['#7F3F98', '#83C341', '#16507B'],
+            'legend' => [
+                'layout' => 'vertical',
+                'align' => 'right',
+                'verticalAlign' => 'middle',
+                'itemMarginTop' => 10,
+                'itemMarginBottom' => 10
+            ],
+            'series'=> $series,
+        ];
+    }
+
+    public function chart_update(){
+        $this->dispatchBrowserEvent("chart_update_$this->chart_id", ['data' => $this->get_data()]);
     }
 }
