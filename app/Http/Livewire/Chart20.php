2a2c8b67fd3b0cd932bfdc95d718ff70df31fcf1
eGen Laptop <sakir.py>
2022-08-24 16:25:30 +0600

    data bind in chart 20

--- app/Http/Livewire/Chart20.php
+++ app/Http/Livewire/Chart20.php
@@ -3,6 +3,9 @@
 namespace App\Http\Livewire;
 
 use App\Models\Chart;
+use App\Models\SecondDatabase\ExportCountry;
+use App\Models\SecondDatabase\ImportCountry;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
@@ -13,15 +16,74 @@ class Chart20 extends Component
 
     public function render()
     {
-        $this->chart =Chart::findOrFail($this->chart_id);
-        if(app()->currentLocale() == 'bn'){
+        $this->chart = Chart::findOrFail($this->chart_id);
+        if (app()->currentLocale() == 'bn') {
             $this->name = $this->chart->bn_name;
             $this->description = $this->chart->bn_description;
-        }else{
+        } else {
             $this->name = $this->chart->en_name;
             $this->description = $this->chart->en_description;
         }
 
         return view('livewire.chart20');
+    }
+
+    public function mount()
+    {
+        $db_data_set = DB::connection('mysql2')->select("SELECT
+        fiscal_year,
+        country,
+        SUM(remittance_in_crore_bdt) AS remittance_in_crore_bdt
+    FROM
+        corona_socio_info.economy_remittance_countrywise
+    WHERE
+        country != 'Total'
+    GROUP BY fiscal_year , country
+    ORDER BY fiscal_year , country DESC");
+
+        $this->fotmated_data_set = array();
+        foreach (collect($db_data_set)->groupBy('fiscal_year') as $fiscal_year => $fiscal_year_wise_data) {
+            array_push($this->fotmated_data_set, [
+                'name' =>  $fiscal_year,
+                'data' =>  $fiscal_year_wise_data->map(function($data){
+                    return ["$data->country".'&nbsp; <img src="'.("/assets/flags/$data->country.png").'" width="20" height="20">', $data->remittance_in_crore_bdt/1000000];
+                }),
+                'color' =>  '#83C341',
+            ]);
+        }
+
+        //Default render by first one or key 0
+        $this->chart_data_set = $this->get_data();
+    }
+
+    public function get_data($selected_key_for_data_view = 0) 
+    {
+        return [
+            'chart'=> [
+                'renderTo'=> 'container',
+                'type'=> 'bar'
+            ],
+            'plotOptions'=> [
+                'column'=> [
+                    'stacking'=> 'normal',
+                    'dataLabels'=> [
+                        'enabled'=> false
+                    ]
+                ]
+            ],
+            'xAxis'=> [
+                'type'=> "category",
+                'labels'=> [
+                    'useHTML'=> true,
+                ],
+            ],
+            'series'=> [$this->fotmated_data_set[$selected_key_for_data_view]]
+        ];
+    }
+
+    protected $listeners = ['change_selected_key_and_chart_update_20'];
+    public function change_selected_key_and_chart_update_20($key)
+    {
+        $this->dispatchBrowserEvent("chart_update_$this->chart_id", ['data' => $this->get_data($key)]);
     }
 }
--- resources/views/livewire/chart20.blade.php
+++ resources/views/livewire/chart20.blade.php
@@ -1,17 +1,61 @@
 <div>
     <div class="card">
-        <div class="card-header">
-            {{ $name }}
+     <div class="card-header">
+         {{ $name }}
+         <div> <button type="button" class="btn btn-trans-icon" wire:click="$emit('editChartInfo', '{{ $chart_id }}')"><i class="bx bx-edit-alt"></i> Edit</button> </div>
+     </div>
+    <div class="card-body">
+     <figure class="highcharts-figure">
+         <div id="chart_id_{{ $chart->id }}"> </div>
+     </figure>
+    <div class="slidecontainer">
+        <input type='range' class="slider" min='0' max='{{ count(collect($fotmated_data_set)->pluck('name')) - 1 }}' step='1' value='0' id="range_chart_id_{{ $chart->id }}" /> 
+        <div class="range-label-container">
+            @foreach (collect($fotmated_data_set)->pluck('name') as $key => $year)
+            <div class="range-label">{{ str_replace("-20", "-", $year) }}</div>
+            @endforeach
         </div>
-        <div class="card-body">
-            <iframe width="100%" height="660px" frameborder="0" allowfullscreen="true" src="https://public.tableau.com/views/sourcewiseremittancedynamicbar/Dashboard1?%3Aembed=y&%3AshowVizHome=no&:device=desktop">
-            </iframe>
-            <p class="text-center">
-                <button type="butto" class="btn btn-secondary btn-sm m-2" wire:click="$emit('editChartInfo', '{{ $chart->id }}')">Edit</button>
-            </p>
-        </div>
-        <div class="card-footer">
-            {!! $description !!}
-        </div>
+        <button type="button" class="btn btn-sm btn-success" id="start_btn_chart_id_{{ $chart->id }}">start</button>
+        <button type="button" class="btn btn-sm btn-warning" id="stop_btn_chart_id_{{ $chart->id }}">stop</button>
     </div>
+    </div>
+     <div class="card-footer">
+         {!! $description !!}
+     </div>
+    </div>
+
+    <script>
+        $(document).ready(function() {
+            //First loaded data
+            Highcharts.chart("chart_id_{{ $chart->id }}", {!! collect($chart_data_set) !!});
+            
+            //chart update and re-render
+            window.addEventListener("chart_update_{{ $chart->id }}", event => {
+                Highcharts.chart("chart_id_{{ $chart->id }}", event.detail.data);
+            });
+
+
+            //Run the update function when the slider is changed
+            d3.select("#range_chart_id_{{ $chart->id }}").on('input', function() {
+                window.livewire.emit('change_selected_key_and_chart_update_20', this.value);
+            });
+
+            let myTimer;
+            d3.select("#start_btn_chart_id_{{ $chart->id }}").on("click", function() {
+            clearInterval (myTimer);
+                myTimer = setInterval (function() {
+                let b= d3.select("#range_chart_id_{{ $chart->id }}");
+                let t = (+b.property("value") + 1) % (+b.property("max") + 1);
+                if (t == 0) { t = +b.property("min"); }
+                b.property("value", t);
+                window.livewire.emit('change_selected_key_and_chart_update_20', t);
+                console.log(t);
+                }, 3000);
+            });
+
+            d3.select("#stop_btn_chart_id_{{ $chart->id }}").on("click", function() {
+                clearInterval (myTimer);
+            });
+        });
+    </script>
 </div>
