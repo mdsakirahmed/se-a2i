7d23c210bf843444806b66ffb81eec1075350cb1
eGen Laptop <sakir.py>
2022-08-30 13:10:09 +0600

    every page title

--- resources/views/backend/economy/overseas-employment-and-remittance.blade.php
+++ resources/views/backend/economy/overseas-employment-and-remittance.blade.php
@@ -5,7 +5,7 @@
         <div class="container">
             <header>
                 <h3>
-                    {{ __('Economy') }}
+                    {{ __('Overseas Employment and Remittance') }}
                 </h3>
             </header>
             <div class="card-lg">
