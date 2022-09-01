7176789a7655861ec08f361448ff9f2c57bc4423
eGen Laptop <sakir.py>
2022-09-01 09:37:59 +0600

    finally  git conflict clear

--- app/Http/Livewire/Chart1.php
+++ app/Http/Livewire/Chart1.php
@@ -42,7 +42,8 @@ class Chart1 extends Component
 
         return [
             'chart' => [
-                'type' => 'column'
+                'type' => 'column',
+                'className' => 'bar-chart'
             ],
 
             'credits' => [
@@ -102,6 +103,13 @@ class Chart1 extends Component
                     'pointWidth'=> 20,
                     'borderRadius' => '8px',
                 ]
+            ],
+            'legend' => [
+                'align' =>'left',
+                'verticalAlign'=> 'top',
+                'layout'=> 'horizontal',
+                'x'=> 0,
+                'y'=> 0
             ],
             'series' => [[
                 'name' => 'Male',
