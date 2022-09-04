032c157194545a49231bbc069bcab0190775706c
eGen Laptop <52326976+mdsakirahmed@users.noreply.github.com>
2022-09-04 10:57:23 +0600

    Chart 16 2021-19 issue fixed

--- app/Http/Livewire/Chart16.php
+++ app/Http/Livewire/Chart16.php
@@ -239,7 +239,7 @@ class Chart16 extends Component
                 'text'=> ''
             ],
             'xAxis'=> [
-                'categories'=> ["2020-21", "2019-20", "2028-19"],
+                'categories'=> ["2020-21", "2019-20", "2018-19"],
                 
             ],
             'yAxis'=> [
