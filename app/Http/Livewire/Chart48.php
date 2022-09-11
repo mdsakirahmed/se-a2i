82e6a953df0bf9c0e92ef295a88bb615923fcfe5
eGen Laptop <52326976+mdsakirahmed@users.noreply.github.com>
2022-09-11 15:17:45 +0600

    work on 48

--- app/Http/Livewire/Chart48.php
+++ app/Http/Livewire/Chart48.php
@@ -2,12 +2,180 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Chart;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class Chart48 extends Component
 {
+    public  Chart $chart;
+    public $name, $description, $datasource, $chart_id = 48;
+    public $years, $districts, $divisions, $selected_year, $selected_district, $selected_division;
+
     public function render()
     {
-        return view('livewire.chart48');
+        $this->chart = Chart::findOrFail($this->chart_id);
+        if (app()->currentLocale() == 'bn') {
+            $this->name = $this->chart->bn_name;
+            $this->description = $this->chart->bn_description;
+            $this->datasource = $this->chart->bn_datasource;
+        } else {
+            $this->name = $this->chart->en_name;
+            $this->description = $this->chart->en_description;
+            $this->datasource = $this->chart->en_datasource;
+        }
+
+        return view('livewire.chart48', [
+            'chart_data_set' => $this->get_data(),
+        ]);
+    }
+
+    public function change_division()
+    {
+        $this->selected_district = null;
+        $this->update_chart();
+    }
+
+    public function update_chart()
+    {
+        $this->dispatchBrowserEvent("chart_update_$this->chart_id", ['data' => $this->get_data()]);
+    }
+
+    public function get_data()
+    {
+
+
+        // $db_data = DB::connection('mysql2')->select("SELECT year, division, district, sum(remittance_in_million_usd) as total_remitance_usd
+        //         FROM corona_socio_info.economy_remittance_districtwise group by year, division, district");
+
+        $db_data = DB::connection('mysql2')->select("SELECT * FROM poverty_yearly_upazilawise");
+
+        $this->years = collect($db_data)->pluck('year')->unique();
+
+        $formated_data = [];
+        foreach (collect($db_data)->groupBy('subdistrict') as $subdistrict => $subdistrict_wise_data) {
+            if ($this->selected_year) {
+                $value = collect($subdistrict_wise_data)->where('year', $this->selected_year)->sum('total_remitance_usd');
+            } else {
+                $value = collect($subdistrict_wise_data)->sum('total_remitance_usd');
+            }
+            // echo(ucwords(strtolower(trans($subdistrict))).": round($value) </br>");
+            array_push($formated_data, [
+                'subdistrict' => ucwords(strtolower(trans($subdistrict))), 
+                'district' =>ucwords(strtolower(trans(collect($subdistrict_wise_data)->first()->district))),
+                'division' => ucwords(strtolower(trans(collect($subdistrict_wise_data)->first()->division))),
+                'value' => round($value)
+            ]);
+        }
+
+        $this->divisions = collect($formated_data)->pluck('division')->unique();
+        $this->districts = collect($formated_data)->pluck('district')->unique();
+
+        // if ($this->selected_division) {
+        //     $this->subdistricts = collect($formated_data)->where('division', $this->selected_division)->pluck('subdistrict');
+        // } else {
+        //     $this->subdistricts = collect($formated_data)->pluck('subdistrict');
+        // }
+
+        //Get data from json file
+        $geojson = json_decode(file_get_contents(public_path('assets/json/bangladesh.geojson.json')), true);
+
+        //Filter data
+        // $filter_geojson = $geojson;
+        // $filter_geojson['features'] = [];
+        // foreach ($geojson['features'] as $feature) {
+        //     if ($this->selected_district && $this->selected_division) {
+        //         if ($feature['properties']['district'] == $this->selected_district && $feature['properties']['division'] == $this->selected_division) {
+        //             array_push($filter_geojson['features'], $feature);
+        //         }
+        //     } else if ($this->selected_district && !$this->selected_division) {
+        //         if ($feature['properties']['district'] == $this->selected_district) {
+        //             array_push($filter_geojson['features'], $feature);
+        //         }
+        //     } else if (!$this->selected_district && $this->selected_division) {
+        //         if ($feature['properties']['division'] == $this->selected_division) {
+        //             array_push($filter_geojson['features'], $feature);
+        //         }
+        //     } else {
+        //         array_push($filter_geojson['features'], $feature);
+        //     }
+        // }
+        // $geojson = $filter_geojson;
+
+        //Make map data set
+        return [
+            'chart' => [
+                'map' => collect($geojson['features'])
+            ],
+
+            'credits' => [
+                'enabled' => false
+            ],
+
+            'title' => [
+                'text' => ""
+            ],
+
+            'accessibility' => [
+                'typeDescription' => ""
+            ],
+            'legend' => [
+                'enabled' => false
+              ],
+            'mapNavigation' => [
+                'enabled' => true,
+                'buttonOptions' => [
+                    'verticalAlign' => "bottom"
+                ]
+            ],
+            'tooltip' => [
+                'useHTML' => true,
+                'headerFormat' => '',
+                'pointFormat' => 'Division: {point.division}<br>District: {point.district}<br>Remittance (In Million Us$): {point.value:,.2f}',
+                'style' => [
+                    'color' => '#fff'
+                ],
+                'valueDecimals' => 0,
+                'backgroundColor' => '#444444',
+                'borderColor' => '#eeee',
+                'borderRadius' => 10,
+                'borderWidth' => 3,
+            ],
+
+            'colorAxis' => [
+                'tickPixelInterval' => 100,
+                'min' => 1,
+                'max' => 10,
+                'type' => 'logarithmic',
+                'minColor' => '#cfc5d4',
+                'maxColor' => '#7F3F98'
+            ],
+
+            'series' => [
+                [
+                    'data' => collect($formated_data)->map(function ($data) {
+                        return [$data['division'], $data['district'], $data['subdistrict'], $data['value']];
+                    }),
+                    'keys' => ["division", "district", "subdistrict", "value"],
+                    'joinBy' => ['upazila', 'subdistrict'],
+                    'name' => "Remittance In Million USD",
+                    'states' => [
+                        'hover' => [
+                            'color' => "#9cc13d"
+                        ]
+                    ],
+                    'dataLabels' => [
+                        'enabled' => true,
+                        'format' => "{point.properties.subdistrict}",
+                        'style' => [
+                            'textShadow' => false,
+                            'strokeWidth' => 0,
+                            'textOutline' => false,
+                            'color' => '#323232'
+                        ]
+                    ]
+                ]
+            ]
+        ];
     }
 }
