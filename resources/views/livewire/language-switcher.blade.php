d5141b2f870f15282077d673cb20c917c777bbb6
eGen Laptop <52326976+mdsakirahmed@users.noreply.github.com>
2022-09-11 19:18:17 +0600

    Update language-switcher.blade.php

--- resources/views/livewire/language-switcher.blade.php
+++ resources/views/livewire/language-switcher.blade.php
@@ -1,1 +1,13 @@
-<a href="#" wire:click="change_language">{{ __('Change Language') }}</a>
+<div>
+    <div class="lang-text">
+        <a href="#" wire:click="change_language">
+            <div class="language_switcher_text">{{ __('Change Language') }}</div>
+            @if(App::isLocale('en'))
+            <img class="language_switcher_image" src="{{ asset('assets/img/uk-flag.png') }}" />
+            @else
+            <img class="language_switcher_image" src="{{ asset('assets/img/bd-flag.png') }}" />
+            @endif
+        </a>
+    </div>
+   
+</div>
