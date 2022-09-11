ced722801cd66877a176775ad47589836a460906
sanjida_1264 <sanjida.islam@egeneration.co>
2022-09-11 17:11:13 +0600

    barishal spelling change

--- resources/views/livewire/chart19.blade.php
+++ resources/views/livewire/chart19.blade.php
@@ -31,7 +31,7 @@
         <select wire:model="selected_division" wire:change="change_divition">
             <option value="">All division</option>
             <option value="Khulna">Khulna</option>
-            <option value="Barisal">Barisal</option>
+            <option value="Barishal">Barishal</option>
             <option value="Rajshahi">Rajshahi</option>
             <option value="Chittagong">Chittagong</option>
             <option value="Dhaka">Dhaka</option>
