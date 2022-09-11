b85240d83d76ae939cc2910224c1af9b0b666e7e
eGen Laptop <52326976+mdsakirahmed@users.noreply.github.com>
2022-09-11 14:03:31 +0600

    Working on District-wise Remittance over the Years

--- app/Http/Livewire/Chart17.php
+++ app/Http/Livewire/Chart17.php
@@ -56,8 +56,9 @@ class Chart17 extends Component
             } else {
                 $value = collect($district_wise_data)->sum('total_remitance_usd');
             }
+            echo(ucwords(strtolower(trans($district))).": round($value) </br>");
             array_push($formated_data, [
-                'district' => ucfirst(strtolower(trans($district))), 'value' => round($value), 'division' => ucfirst(strtolower(trans(collect($district_wise_data)->first()->division)))
+                'district' => ucwords(strtolower(trans($district))), 'value' => round($value), 'division' => ucwords(strtolower(trans(collect($district_wise_data)->first()->division)))
             ]);
         }
 
